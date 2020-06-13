e4a345241cb5358e8b43de0f49edfc6a8589f3e5
Yury Gribov <tetra2005@gmail.com>
2020-06-13 11:59:20 +0300

    Disable vtables test for non-amd64 targets.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -22,5 +22,5 @@ export PYTHON="${PYTHON:-python3}"
 tests/basic/run.sh $ARCH
 tests/exceptions/run.sh $ARCH
 tests/data-warnings/run.sh $ARCH
-tests/vtables/run.sh $ARCH
+test -n "$ARCH" || tests/vtables/run.sh $ARCH  # TODO: enable for other arches
 test -n "$ARCH" || tests/ld/run.sh  # TODO: enable for other targets
