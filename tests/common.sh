6d9d46b8aec65231f5cb172f1e9939d235b9ebf0
Yuri Gribov <tetra2005@gmail.com>
2022-01-18 23:12:33 +0300

    Support non-x86 native testing.

--- tests/common.sh
+++ tests/common.sh
@@ -7,47 +7,51 @@ if test -z "${ARCH:-}"; then
 fi
 
 CFLAGS="-Wall -Wextra -Werror ${CFLAGS:-}"
-case "${ARCH:-}" in
-arm*hf | armhf-*)
-  # To run tests for ARM install
-  # $ sudo apt-get install gcc-arm-linux-gnueabi qemu-user
-  TARGET=armhf
-  PREFIX=arm-linux-gnueabihf-
-  INTERP="qemu-arm -L /usr/arm-linux-gnueabihf -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-  CFLAGS="$CFLAGS -mthumb-interwork"
-  ;;
-arm | arm-* | armel-*)
-  # To run tests for ARM install
-  # $ sudo apt-get install gcc-arm-linux-gnueabi qemu-user
-  TARGET=arm
-  PREFIX=arm-linux-gnueabi-
-  INTERP="qemu-arm -L /usr/arm-linux-gnueabi -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-  ;;
-aarch64*)
-  # To run tests for AArch64 install
-  # sudo apt-get install gcc-aarch64-linux-gnu qemu-user
-  TARGET=aarch64
-  PREFIX=aarch64-linux-gnu-
-  INTERP="qemu-aarch64 -L /usr/aarch64-linux-gnu -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-  ;;
-i[0-9]86*)
-  # To run tests for AArch64 install
-  # sudo apt-get install gcc-aarch64-linux-gnu qemu-user
-  TARGET=i686
-  PREFIX=
-  INTERP=
-  CFLAGS="$CFLAGS -m32"
-  ;;
-'' | x86_64* | host)
+
+if test $ARCH = $(uname -m); then
+  # Native
   TARGET=x86_64
   PREFIX=
   INTERP=
-  ;;
-*)
-  echo >&2 "Unsupported target: $ARCH"
-  exit 1
-  ;;
-esac
+else
+  # Simulate
+  case "$ARCH" in
+  arm*hf | armhf-*)
+    # To run tests for ARM install
+    # $ sudo apt-get install gcc-arm-linux-gnueabi qemu-user
+    TARGET=armhf
+    PREFIX=arm-linux-gnueabihf-
+    INTERP="qemu-arm -L /usr/arm-linux-gnueabihf -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    CFLAGS="$CFLAGS -mthumb-interwork"
+    ;;
+  arm | arm-* | armel-*)
+    # To run tests for ARM install
+    # $ sudo apt-get install gcc-arm-linux-gnueabi qemu-user
+    TARGET=arm
+    PREFIX=arm-linux-gnueabi-
+    INTERP="qemu-arm -L /usr/arm-linux-gnueabi -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    ;;
+  aarch64*)
+    # To run tests for AArch64 install
+    # sudo apt-get install gcc-aarch64-linux-gnu qemu-user
+    TARGET=aarch64
+    PREFIX=aarch64-linux-gnu-
+    INTERP="qemu-aarch64 -L /usr/aarch64-linux-gnu -E LD_LIBRARY_PATH=.${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+    ;;
+  i[0-9]86*)
+    # To run tests for x86 install
+    # sudo apt-get install gcc-multilib g++-multilib
+    TARGET=i686
+    PREFIX=
+    INTERP=
+    CFLAGS="$CFLAGS -m32"
+    ;;
+  *)
+    echo >&2 "Unsupported target: $ARCH"
+    exit 1
+    ;;
+  esac
+fi
 
 CC=$PREFIX${CC:-gcc}
 CXX=$PREFIX${CXX:-g++}
