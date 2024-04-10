305ee2824e307d9ec6a541b8b84c57246bf3f3fb
Yury Gribov <tetra2005@gmail.com>
2024-04-10 23:39:34 +0300

    Minor test update.

--- tests/stack-args/main.c
+++ tests/stack-args/main.c
@@ -11,18 +11,23 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+static void usage(char *progname) {
+  fprintf(stderr, "Usage: %s float/int\n", progname);
+  exit(1);
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2)
-    return 1;
+    usage(argv[0]);
 
   if (0 == strcmp(argv[1], "int")) {
     foo(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
   } else if (0 == strcmp(argv[1], "float")) {
     bar(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
   } else {
-    fprintf(stderr, "Invalid option: %s\n", argv[1]);
-    return 1;
+    usage(argv[0]);
   }
 
   return 0;
