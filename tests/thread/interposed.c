634a8380ea15ccb9551bd34010aa28516ea6ce38
Yury Gribov <tetra2005@gmail.com>
2025-01-19 08:04:44 +0300

    Various test improvements.

--- tests/thread/interposed.c
+++ tests/thread/interposed.c
@@ -10,52 +10,59 @@
 #include <stdio.h>
 #include "interposed.h"
 
+static int glob;
+
+__attribute__((constructor))
+static void init() {
+  glob = foo0(0) + foo1(0);
+}
+
 __attribute__((visibility("default")))
 int foo0(int x) {
-  return x + 0;
+  return glob + x + 0;
 }
 
 __attribute__((visibility("default")))
 int foo1(int x) {
-  return x + 1;
+  return glob + x + 1;
 }
 
 __attribute__((visibility("default")))
 int foo2(int x) {
-  return x + 2;
+  return glob + x + 2;
 }
 
 __attribute__((visibility("default")))
 int foo3(int x) {
-  return x + 3;
+  return glob + x + 3;
 }
 
 __attribute__((visibility("default")))
 int foo4(int x) {
-  return x + 4;
+  return glob + x + 4;
 }
 
 __attribute__((visibility("default")))
 int foo5(int x) {
-  return x + 5;
+  return glob + x + 5;
 }
 
 __attribute__((visibility("default")))
 int foo6(int x) {
-  return x + 6;
+  return glob + x + 6;
 }
 
 __attribute__((visibility("default")))
 int foo7(int x) {
-  return x + 7;
+  return glob + x + 7;
 }
 
 __attribute__((visibility("default")))
 int foo8(int x) {
-  return x + 8;
+  return glob + x + 8;
 }
 
 __attribute__((visibility("default")))
 int foo9(int x) {
-  return x + 9;
+  return glob + x + 9;
 }
